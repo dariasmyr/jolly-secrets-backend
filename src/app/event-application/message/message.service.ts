c541724944901c11985af9b891778b0ac963d567
Daria Smyr <68226469+dariasmyr@users.noreply.github.com>
2023-11-27 08:36:04 +0000

    Update message.service.ts

--- src/app/event-application/message/message.service.ts
+++ src/app/event-application/message/message.service.ts
@@ -8,16 +8,24 @@ import { PrismaService } from '@/common/prisma/prisma.service';
 export class MessageService {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async createMessage(input: CreateMessageInput): Promise<Message | null> {
-    const { chatId, text, accountId } = input;
-    return this.prismaService.message.create({
-      data: {
-        chatId,
-        text,
-        accountId,
-      },
-    });
+  async function createMessage(input: CreateMessageInput): Promise<Message | null> {
+  const { chatId, text, accountId } = input;
+  if (text.trim() === '') {
+    return null;
   }
+
+  if (text.length > 200) {
+    return null;
+  }
+
+  return this.prismaService.message.create({
+    data: {
+      chatId,
+      text,
+      accountId,
+    },
+  });
+}
 
   async getMessageByChatId(chatId: number): Promise<Array<Message> | null> {
     return this.prismaService.message.findMany({
